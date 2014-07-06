7fe7e7de5e7ea5a9adb6e381bc908a5a8c958722
prime number <primenumber.proj@gmail.com>
2014-07-07 00:59:01 +0900

    Create tanzaku.rb

---
+++ tanzaku.rb
@@ -0,0 +1,29 @@
+#-*- coding: utf-8
+
+Plugin.create(:tanzaku) do
+  UserConfig[:tanzaku_ni_suru] ||= true
+
+  settings "短冊" do
+    boolean '短冊にする', :tanzaku_ni_suru
+  end 
+  filter_gui_postbox_post do |gui_postbox|
+    buf = Plugin.create(:gtk).widgetof(gui_postbox).widget_post.buffer
+    text = buf.text
+    if UserConfig[:tanzaku_ni_suru]
+      to = nil
+      if /^@[a-zA-Z0-9_]* / =~ text
+         tmp = text.split(" ", 2)
+         to = tmp[0]
+         text = tmp[1]
+      end
+      str = ""
+      str = to unless to.nil?
+      str += "\n" unless to.nil?
+      str += "┏┷┓\n┃　┃\n┃"
+      text.each_char {|char| str += char + "┃\n┃"}
+      str += "　┃\n┗━┛\n"
+      buf.text = str
+    end
+    [gui_postbox]
+  end
+end
